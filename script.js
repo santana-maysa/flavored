d1941863f8f5da901e89fba99ddd59783a2c9c23
Maysa Santana <santanamaysa868@gmail.com>
2025-07-11 14:21:32 -0300

    alterações e correções

--- script.js
+++ script.js
@@ -19,3 +19,25 @@ next.addEventListener('click', () => {
     behavior: 'smooth'
   });
 });
+
+function verificarHorario() {
+  const agora = new Date();
+  const hora = agora.getHours();
+  const status = document.getElementById('status-cafe');
+
+  if (hora >= 8 && hora < 19) {
+    status.textContent = "Estamos abertos agora! Venha nos visitar.";
+    status.style.color = "green";
+  } else {
+    status.textContent = "Estamos fechados no momento. Horário: 08h - 19h";
+    status.style.color = "red";
+  }
+}
+
+verificarHorario();
+
+
+const btnTopo = document.querySelector('.btn-topo');
+window.addEventListener('scroll', () => {
+  btnTopo.classList.toggle('show', window.scrollY > 300);
+});
